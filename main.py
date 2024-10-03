0ad0ffb9b5b4d483ee6665a2cc65cc0167b3763c
Raghav <51016731+Raghav-Portfolio@users.noreply.github.com>
2024-10-03 09:28:01 +0530

    Updated main.py to explain why .split(';') is needed

--- main.py
+++ main.py
@@ -26,5 +26,16 @@ while True:
     folder=values['folder']
     zip_function.make_archive(filepaths, folder)
     window['output'].update(value='Compress Successful!')
-    
+
+'''
+the split(';') is used to separate multiple file paths returned by the sg.FilesBrowse
+element (the "Choose" button) when a user selects multiple files.
+Why .split(';') is Necessary:
+When using the FilesBrowse element in PySimpleGUI, if the user selects multiple files, 
+they are returned as a single string where each file path is separated by a semicolon (;).
+To work with each file individually, you need to split this string into a list of file paths. 
+The semicolon acts as the separator between the paths, and .split(';') breaks the string into individual file paths, 
+allowing you to iterate over them or process them separately.
+'''
+
 window.close()
